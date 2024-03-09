0dd7e9dd655ec9dec97aac4133279b6f845daa49
antarikshgosain <antariksh.gosain@gmail.com>
2024-03-09 01:28:01 -0500

    array filter method

--- js-refresher/workshop/array.js
+++ js-refresher/workshop/array.js
@@ -1,12 +1,15 @@
 const hobbies = ['Sports', 'Reading', 1234, true];
 
-for(let hobby of hobbies){
-    console.log(hobby);
-}
+// for(let hobby of hobbies){
+//     console.log(hobby);
+// }
+console.log(hobbies);
 
 const newHobbies = hobbies.map((hobby) => "Hobby: " + hobby);
 
+console.log(newHobbies);
 
-for(let hobby of newHobbies){
-    console.log(hobby);
-}
+const filteredHobbies = hobbies.filter((hobby) => typeof hobby === 'string');
+
+console.log(filteredHobbies)
+
